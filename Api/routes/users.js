370aef2157b730404483c192bc532d971535798f
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2023-01-04 17:00:15 -0300

    done integration

--- Api/routes/users.js
+++ Api/routes/users.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUsers } from "../controllers/user";
+import { getUsers } from "../controllers/user.js";
 
 const router = express.Router();
 router.get("/", getUsers);
