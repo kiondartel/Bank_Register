409898c3b5cdb116254a3e4757ddb55f1278cd3f
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2023-01-09 16:45:01 -0300

    delete users

--- Api/controllers/user.js
+++ Api/controllers/user.js
@@ -45,3 +45,13 @@ export const updateUser = (req, res) => {
     return res.status(200).json("Usuario Atualiado com sucesso");
   });
 };
+
+export const deleteUser = (req, res) => {
+  const q = "DELETE FROM users WHERE `id` = ?";
+
+  db.query(q, [req.params.id], (err) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json("Usuario deletado com sucesso");
+  });
+};
